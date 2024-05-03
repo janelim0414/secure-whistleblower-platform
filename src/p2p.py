9df295efbec362bb60d224921933bf2391703578
janelim0414 <janelim801@gmail.com>
2024-05-03 14:28:46 -0400

    fork testing

--- src/p2p.py
+++ src/p2p.py
@@ -34,8 +34,12 @@ class MsgQueue:
     def __init__(self):
         self.queue = queue.Queue()
 
-    def put_msg(self, block):
-        self.queue.put(block)
+    def put_msg(self, msg, dst):
+        """
+        given a message and a destination ip address (peer),
+        create a tuple and push it to queue for the specified peer to process (create block and add)
+        """
+        self.queue.put((msg, dst))
 
     def get_msg(self):
         return self.queue.get()
@@ -88,24 +92,35 @@ class PeerNetwork:
         receive data from each peer/client connection
         """
         while True:
-            new_block = client_socket.recv(1024).decode()  # receive data from peer
-            if new_block == "":  # peer has disconnected
+            header = client_socket.recv(1).decode()  # receive data from peer
+            if header == "":  # peer has disconnected
                 client_socket.close()
                 self.recv_sockets.remove(client_socket)
                 print(f"Client disconnected: {client_socket}")
                 break
-            new_block = json.loads(new_block)
-            new_block = Block(**new_block)
-            new_block_hash = new_block.mine('0000')
-            try:
-                # add incoming block to this blockchain
-                self.blockchain.add_block(new_block, new_block_hash)
-            except Exception as e:
-                print(e)
+            if header == "c":  # chain received from peers
+                chain = client_socket.recv(1024).decode() 
+                chain = json.loads(chain)
+                chain = Blockchain(**chain)
+                if len(chain.chain) > len(self.blockchain.chain):
+                    self.blockchain = chain  # update chain with longest 
+            if header == "b":  # new block added by other peer
+                new_block = client_socket.recv(1024).decode() 
+                new_block = json.loads(new_block)
+                new_block = Block(**new_block)
+                try:
+                    # add incoming block to this blockchain
+                    self.blockchain.add_block(new_block, new_block.curr_hash)
+                except Exception as e:
+                    print(e)
             print("current chain:")
             self.blockchain.print_chain()
 
     def _nodeTrackerComm(self):
+        """
+        communicate with traker to update list of peers across network
+        handle newly joined peers by sending this node's blockchain
+        """
         while True:
             decoded_data = self.socket.recv(1024).decode()  # receive data from tracker
             # handle query from tracker
@@ -118,11 +133,18 @@ class PeerNetwork:
                 print(f"received from tracker: {new_peers}")
                 for peer in new_peers:
                     if (not peer in self.peers) and (peer != self.ip):
+                        # create send channel from this node to newly joined node
                         s = socket(AF_INET, SOCK_STREAM)
                         s.connect((peer, self.port)) 
                         print(f"send channel connected from: {peer}")
                         self.peer_sockets.append(s)
+                        # send chain of this node to newly joined node
+                        chain_data = json.dumps(self.blockchain.__dict__, default=datetime_serializer)
+                        header = "c".encode()  # "c" for chain
+                        s.sendall(header + chain_data.encode())
+                        # start thread for sending blocks for this peer
                         threading.Thread(target=self._send, args=(s,)).start()
+                # update list of peers
                 new_peers.remove(self.ip)
                 self.peers = new_peers
                 print(f"updated list of peers: {self.peers}\n")
@@ -133,13 +155,24 @@ class PeerNetwork:
         """
         while True:
             if not self.msg_q.queue_empty():
-                msg = self.msg_q.get_msg()  # retreive message from queue
+                msg = self.msg_q.get_msg()  # retreive put request from queue
+                if msg[1] != self.ip:  # if the req is not for this node, skip and wait for more
+                    continue
+                # add block to this node's chain
                 last_block = self.blockchain.get_last_block()
                 last_block.print_block()
                 new_block = Block(last_block.block_number + 1, msg, last_block.prev_hash)  # create block from message
+                new_hash = new_block.mine('0000')  # get hash to verify block
+                try:
+                    self.blockchain.add_block(new_block, new_hash)  # add to this node's chain as requested
+                except Exception as e:
+                    print(e)
+                # send block to peers
                 block_to_send = json.dumps(new_block.__dict__, default=datetime_serializer)
+                header = "b".encode()  # "b" for block
+                # TODO check if send needs to be kept in one thread 
                 print(f"sending new block from {self.ip} to {send_sock}")
-                send_sock.sendall(block_to_send.encode())  # send given data to peers  
+                send_sock.sendall(header + block_to_send.encode())  # send block to peers  
 
     def _handleTracker(self):
         if self.tracker_addr != self.ip:
@@ -207,9 +240,9 @@ class PeerNetwork:
         
 
 if __name__ == "__main__":
-    tracker_port = int(sys.argv[1]) # the port used to send messages to neighbors
+    tracker_port = int(sys.argv[1])
     tracker_addr = sys.argv[2]
-    is_tracker = bool(int(sys.argv[3])) # the port used to send messages to neighbors
+    is_tracker = bool(int(sys.argv[3]))
 
     def run_p2p_net(msg_q):
         PeerNetwork(is_tracker, tracker_addr, tracker_port, msg_q)
@@ -221,6 +254,6 @@ if __name__ == "__main__":
 
     print("here")
     if not is_tracker:
-        msg_q.put_msg('block 1 data')
+        msg_q.put_msg('block 1 data from ', '')
 
 
